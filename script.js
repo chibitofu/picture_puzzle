316471d977c6424dfc58c62897443e7e9a36ba91
chibitofu <erinmoony@gmail.com>
2016-08-02 21:06:07 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function gameBoard(tiles) {
+  
+}
+
+function reveal() {
+  var tiles = $('.tiles');
+  // var randomTile = shuffle(items).slice(0,1);
+  // var tileCSS = randomTile.css();
+  // console.log(randomTile);
+  // console.log(tileCSS);
+  console.log(tiles);
+}
