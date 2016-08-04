f67baf36863fcacfbe32293eeb75d09cb95d1083
chibitofu <erinmoony@gmail.com>
2016-08-03 17:41:27 -0700

    Basic board.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function gameBoard(tiles) {
-  
+  for (var i = 0; i < tiles; i++) {
+    document.getElementById('board').innerHTML += '<div class="row"></div>';
+  }
+  for (var j = 0; j < tiles; j++) {
+    $(".row").append('<div class="tile">C</div>');
+  }
 }
+
+gameBoard(5);
 
 function reveal() {
   var tiles = $('.tiles');
